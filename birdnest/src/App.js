afdc3cb736483eb491df5b3d0c89e89ac26caa9b
Joel <xxstokexfanxx@gmail.com>
2022-12-06 00:46:37 +0200

    parsing drone data

--- birdnest/src/App.js
+++ birdnest/src/App.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import XMLDrones from "./services/XMLDrones.js";
 import XMLParser from 'react-xml-parser';
-import DroneData from "./services/XMLDrones";
 
 function App() {
   const [droneData, setDroneData] = useState('');
+
   useEffect(() => {
-    setDroneData(DroneData());
-  }, [])
+    XMLDrones.DroneData().then(response => {
+      var xml = new XMLParser().parseFromString(response)
+      setDroneData(xml.children['1'].children);
+      console.log(xml.children['1'].children)
+    });
+  }, []);
   return (
     <div>
       <p>Hello world</p>
-      {droneData}
+      <ul>
+        {Object.keys(droneData).map(drone => {
+          <li key={droneData[drone].children[0].value}>{droneData[drone].children[0].value}</li>
+          console.log(droneData[drone].children[0].value)
+          console.log(droneData[drone].children['7'].value) // Y-coordinate
+          console.log(droneData[drone].children['8'].value) // X-coordinate
+        })}
+      </ul>
     </div>
   );
 }
--- birdnest/src/services/XMLDrones.js
+++ birdnest/src/services/XMLDrones.js
@@ -5,18 +5,14 @@ import XMLParser from 'react-xml-parser';
 //https://droneproxy.fly.dev/https://assignments.reaktor.com/birdnest/drones'
 //https://damp-oasis-94494.herokuapp.com/assignments.reaktor.com/birdnest/drones
 const DroneData = () => {
-  axios
+  const res = axios
     .get('https://droneproxy.fly.dev/https://assignments.reaktor.com/birdnest/drones',
     {
       "Content-Type": "application/xml; charset=utf-8"
-    })
-    .then(response => {
-      const data = response.data;
-      var xml = new XMLParser().parseFromString(data); 
-      const res = xml.children['1'].children
-      console.log(res)
-      return res;
-    })
+    });
+    return res.then(response => response.data);
 };
 
-export default DroneData;
+export default {
+  DroneData: DroneData,
+};
